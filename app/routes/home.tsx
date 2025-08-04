4bd76a3ee880a57db0076e55d5f9b822578a742b
raorohit22 <iamrao@gmail.com>
2025-08-04 14:43:11 +0530

    navbar, and homeage added

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,8 @@
 import type { Route } from "./+types/home";
+import Navbar from "~/components/Navbar";
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
+import { callbackify } from "util";
 
 export function meta({}: Route.MetaArgs) {
 	return [
@@ -10,11 +14,19 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
 	return (
 		<main className="bg-[url('/images/bg-main.svg')] bg-cover">
-			<section className="min-section">
-				<div className="page-heading">
+			<Navbar />
+			<section className="main-section">
+				<div className="page-heading py-16">
 					<h1>Track your Application & Resume Ratings</h1>
 					<h2>Review your submission and check AI-powered feedback</h2>
 				</div>
+				{resumes.length > 0 && (
+					<div className="resumes-section">
+						{resumes.map((resume: any) => (
+							<ResumeCard key={resume.id} resume={resume} />
+						))}
+					</div>
+				)}
 			</section>
 		</main>
 	);
